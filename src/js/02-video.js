6cc7d7e860841ac1bfa63b22ba2aa79f8812eed7
Anton Makhlai <114355602+AntoshaProgram1st@users.noreply.github.com>
2023-04-17 13:12:13 +0300

    push 24

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,8 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
 const iframe = document.querySelector('#vimeo-player');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('play', function () {
   console.log('played the video!');
